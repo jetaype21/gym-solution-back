bec24d63c36b4df9578c9958c9531509eec19497
taypedev <107156051+jetaype21@users.noreply.github.com>
2024-07-12 01:09:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const db = require("./db");
 
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
